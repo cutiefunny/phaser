5f9d1cfb8d64645132e289b755c5b6d076972f24
hansol <hansol.kim@dajit.com>
2025-03-20 15:20:11 +0900

    re

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 443;
+const port = 8000;
 require('dotenv').config();
 const router = require('./router');
 const CRUD= require("./CRUD");
