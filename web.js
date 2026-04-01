a0d88865d2a9cbe0303392c85f6678941abff744
cutiefunny <cutiefunny@gmail.com>
2026-04-01 21:18:23 +0900

    cors 추가

--- web.js
+++ web.js
@@ -55,6 +55,7 @@ const corsOptions = {
     'https://musclecat-sns.vercel.app',
     'https://nyanyapang.com',
     'https://www.nyanyapang.com',
+    'https://musclecat-class.vercel.app',
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
